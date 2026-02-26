0313bf087f4727ff7f96b42890029af5fb78e8dc
Edward Scroop <edward.scroop@gmail.com>
2026-02-26 10:12:20 +0800

    Add json lsp.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,7 @@ return {
         vim.lsp.enable("yamlls")
         vim.lsp.enable("pylsp")
         vim.lsp.enable("systemd_lsp")
+        vim.lsp.enable("jsonls")
 
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
