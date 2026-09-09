87732264897d3cea3b7617ab4222bb09f56d976d
Edward Scroop <edward.scroop@gmail.com>
2026-09-09 19:36:28 +0800

    Change styling.

--- lua/plugins/colour.lua
+++ lua/plugins/colour.lua
@@ -3,18 +3,21 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
+        -- local palette = require("nightfox.palette").load("carbonfox")
+
         require('nightfox').setup({
             options = {
                 styles = {
                     comments = "italic",
                     conditionals = "NONE",
-                    constants = "NONE",
+                    constants = "bold",
                     functions = "NONE",
-                    keywords = "NONE",
-                    numbers = "NONE",
+                    keywords = "bold",
+                    numbers = "italic",
                     operators = "NONE",
-                    strings = "NONE",
-                    types = "NONE",
+                    preprocs = "italic",
+                    strings = "italic",
+                    types = "italic",
                     variables = "NONE",
                 },
             },
@@ -22,20 +25,41 @@ return {
                 carbonfox = {
                     comment = "#af87df",
                     bg0 = "#1d1d2b",
-                    bg1 = "#000000",
+                    bg1 = "black",
                     bg3 = "#121820",
-                    fg2 = "#ffffff",
+                    fg1 = "white",
+                    fg2 = "white",
                     sel0 = "#2a0b42",
                     sel1 = "#3d096e",
                 },
             },
+            -- specs = {
+            --     carbonfox = {
+            --         syntax = {
+            --             type = palette.yellow.base,
+            --             builtin1 = palette.yellow.dim,
+            --             func = palette.blue.base,
+            --             preproc = palette.magenta.base,
+            --             field = palette.cyan.base,
+            --             keyword = palette.magenta.bright,
+            --             const = palette.orange.base,
+            --             builtin0 = palette.red.dim,
+            --             ident = palette.fg1,
+            --             comment = palette.comment,
+            --             string = palette.green.base,
+            --             number = palette.orange.dim,
+            --             operator = palette.fg2,
+            --             bracket = palette.fg3,
+            --         },
+            --     },
+            -- },
             groups = {
                 carbonfox = {
                     ColorColumn = { bg = "#af0000" },
                     LineNrAbove = { fg = "#00ff00" },
                     LineNrBelow = { fg = "#ff0000" },
                     CursorLine = { bg = "#1c1c1c" },
-                    CursorLineNr = { fg = "#ffffff", bg = "#1c1c1c" },
+                    CursorLineNr = { fg = "white", bg = "#1c1c1c" },
                 },
             },
         })
